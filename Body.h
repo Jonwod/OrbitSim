73e4f8f7515af09377b0b9ae547977f26155eeae
Jonathan Wood <jonathan.m.wood95@gmail.com>
2018-12-31 16:26:21 +0000

    Added const specifier

--- Body.h
+++ Body.h
@@ -10,7 +10,7 @@ public:
 	//Steps this body forward by dt seconds
 	void step(float dt);
 	inline void setPosition(const Vec2& newPosition) { _visual.setPosition(newPosition); }
-	inline const Vec2& getPosition() { return _visual.getPosition(); };
+	inline const Vec2& getPosition() const { return _visual.getPosition(); };
 	inline void setVelocity(const Vec2& newVelocity) { _velocity = newVelocity; }
 	inline void draw(sf::RenderWindow& window) { window.draw(_visual); }
 private:
