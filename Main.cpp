acc6b27c2007bd76fd13288434083201acb9a4a8
Jonathan Wood <jonathan.m.wood95@gmail.com>
2018-12-31 19:55:40 +0000

    Added Max Error to HUD

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,7 @@
 #include "VectorMath.h"
 
 
-constexpr float gravityStrength = 2.f;
+constexpr float gravityStrength = 3.f;
 
 
 // Returns the scalar speed for body to orbit other_body in a circle
@@ -41,10 +41,12 @@ public:
 		startRadiusText.setCharacterSize(14);
 		currentRadiusText.setCharacterSize(14);
 		errorText.setCharacterSize(14);
+		maxErrorText.setCharacterSize(14);
 
 		startRadiusText.setPosition({10.f, 0.f});
 		currentRadiusText.setPosition({ 10.f, 20.f});
 		errorText.setPosition({10.f, 40.f});
+		maxErrorText.setPosition({ 10.f, 60.f });
 
 		startRadiusText.setString("start radius: " + std::to_string(_startRadius));
 	}
@@ -52,8 +54,11 @@ public:
 
 	void update(const Body& orbiter, const Body& centerBody) {
 		const float radius = VMath::magnitude(orbiter.getPosition() - centerBody.getPosition());
+		const float error = radius - _startRadius;
+		_maxError = std::max(std::fabs(error), _maxError);
 		currentRadiusText.setString("current radius: " + std::to_string(radius));
-		errorText.setString("error : " + std::to_string(radius - _startRadius));
+		errorText.setString("error : " + std::to_string(error));
+		maxErrorText.setString("max error: " + std::to_string(_maxError));
 	}
 
 
@@ -61,14 +66,17 @@ public:
 		window.draw(startRadiusText);
 		window.draw(currentRadiusText);
 		window.draw(errorText);
+		window.draw(maxErrorText);
 	}
 
 	sf::Text startRadiusText{"start radius: ", _font};
 	sf::Text currentRadiusText{"current radius: ", _font};
 	sf::Text errorText{"error: ", _font};
+	sf::Text maxErrorText{"max error: ", _font};
 private:
 	sf::Font _font;
 	const float _startRadius;
+	float _maxError = 0.f;
 };
 
 //
